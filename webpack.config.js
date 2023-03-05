faa52ac0170d5bef55b633a3e45a0ec4cd4b92e1
shanushka <anushkashrestha24@gmail.com>
2023-03-04 22:39:53 -0600

    yaml ignore

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js"},
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js", publicPath: '/'},
   mode: process.env.NODE_ENV || "development",
   resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
   devServer: { contentBase: path.join(__dirname, "src") },
@@ -25,6 +25,11 @@ module.exports = {
     ],
   },
   plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': '/',
+    }),
+    
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "src", "index.html"),
       filename: "index.html",
