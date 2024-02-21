44b3d51357fb2d64006a16aedafa002f54ed5569
shanushka <anushkashrestha24@gmail.com>
2024-02-20 21:33:39 -0500

    Changes in webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -30,8 +30,6 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: 'assets/images/[name].[ext]', // Adjust path as needed
-          publicPath: 'dist/',
-          outputPath: 'dist/'
         }
       }
     ],
